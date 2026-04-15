6614261cf340dc8d591aa859cbdcb01ead76c73b
Sayed MD Safiuddin Safi <safiuddin102030@gmail.com>
2026-04-16 02:41:28 +0600

    fix: add serviceName to CRM module for Medusa compatibility

--- src/modules/crm/index.ts
+++ src/modules/crm/index.ts
@@ -3,4 +3,7 @@ import * as Models from "./models"
 
 export default {
   service: CrmModuleService,
+  key: "crm",
+  // Added serviceName to satisfy Medusa's module requirements
+  serviceName: "crm",
 }
